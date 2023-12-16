06c5d8142fc55bc385d7a57af79af064a256fb0f
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-12-16 22:43:35 +0400

    diary2

---
+++ src/components/AuthorizedUser/DiaryPage/Lunch.js
@@ -0,0 +1,109 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addMeal } from 'redux/diarySlice';
+import style from './DiaryPage.styled.jsx';
+
+const Lunch = () => {
+  const dispatch = useDispatch();
+  const meals = useSelector((state) => state.diary.meals);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [mealName, setMealName] = useState('');
+  const [carbonohidrates, setCarbonohidrates] = useState('');
+  const [protein, setProtein] = useState('');
+  const [fat, setFat] = useState('');
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setMealName('');
+    setCarbonohidrates('');
+    setProtein('');
+    setFat('');
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(
+      addMeal({
+        name: mealName,
+        carbonohidrates,
+        protein,
+        fat,
+      })
+    );
+    closeModal();
+  };
+
+  return (
+    <div className={style.section}>
+      <h2>Lunch</h2>
+      <button onClick={openModal}>+ Record your meal</button>
+
+      {modalOpen && (
+        <div className={style.modal}>
+          <h2>Lunch</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="mealName">The name of the product or dish</label>
+            <input
+              type="text"
+              id="mealName"
+              value={mealName}
+              onChange={(e) => setMealName(e.target.value)}
+              required
+            />
+
+            <label htmlFor="carbonohidrates">Carbonohidrates</label>
+            <input
+              type="text"
+              id="carbonohidrates"
+              value={carbonohidrates}
+              onChange={(e) => setCarbonohidrates(e.target.value)}
+              required
+            />
+
+            <label htmlFor="protein">Protein</label>
+            <input
+              type="text"
+              id="protein"
+              value={protein}
+              onChange={(e) => setProtein(e.target.value)}
+              required
+            />
+
+            <label htmlFor="fat">Fat</label>
+            <input
+              type="text"
+              id="fat"
+              value={fat}
+              onChange={(e) => setFat(e.target.value)}
+              required
+            />
+            <button type="submit" onSubmit={handleSubmit}>
+              delete
+            </button>
+            <button type="submit" onSubmit={handleSubmit}>
+              + Add more
+            </button>
+            <button type="submit">Save</button>
+            <button type="button" onClick={closeModal}>
+              Cancel
+            </button>
+          </form>
+        </div>
+      )}
+      {meals.map((meal) => (
+        <div key={meal.id}>
+          <p>{meal.name}</p>
+          <p>Carbonohidrates: {meal.carbonohidrates}</p>
+          <p>Protein: {meal.protein}</p>
+          <p>Fat: {meal.fat}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Lunch;
--- src/pages/Authorized/DiaryPage/DiaryPage.jsx
+++ src/pages/Authorized/DiaryPage/DiaryPage.jsx
@@ -1,7 +1,19 @@
+import { Route, Switch } from 'react-router-dom';
+import Breakfast from './Breakfast';
+import Lunch from './Lunch';
+import Dinner from './Dinner';
+import Snack from './Snack';
+
 const DiaryPage = () => {
   return (
     <div>
-      <h1>Diary Page!</h1>
+      <h1>Diary Page</h1>
+      <Switch>
+        <Route path="/diary/breakfast" component={Breakfast} />
+        <Route path="/diary/lunch" component={Lunch} />
+        <Route path="/diary/dinner" component={Dinner} />
+        <Route path="/diary/snack" component={Snack} />
+      </Switch>
     </div>
   );
 };
