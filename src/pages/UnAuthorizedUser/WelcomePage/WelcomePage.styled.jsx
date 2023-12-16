4c9641925210d8db3a435e523b82e8547e85b097
opoberezh <opoberezh.2015@gmail.com>
2023-12-16 21:05:48 +0100

    media_rules

--- src/pages/UnAuthorizedUser/WelcomePage/WelcomePage.styled.jsx
+++ src/pages/UnAuthorizedUser/WelcomePage/WelcomePage.styled.jsx
@@ -1,30 +1,54 @@
 import styled from 'styled-components';
+import { device } from '../../../Device';
 
 export const WelcomeContainer = styled.div`
+border: 2px solid red;
 padding: 10px;
-max-width: 300px;
+@media ${device.tablet} { 
+  max-width: 1200px;
+  padding: 20px;
+}
+
+@media ${device.desktop} {
+  max-width: 1376px;
+  padding: 32px;
+}
+
 `;
 
 export const HeroImg = styled.img`
-width: 100%;
-height: auto;
+  width: 100%;
+  height: auto;
   margin: 0;
 `;
 
 export const Wrapper = styled.div`
-// width: 100%;
-text-align: start;
-//   display: flex;
-//   flex-direction: column;
-//   margin-top: 24px;
-//   margin-bottom: 40px;
-// //   box-sizing: border-box;
-//   max-width: 300px;
+  width: 100%;
+  text-align: start;
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+  margin-bottom: 40px;
+  //   box-sizing: border-box;
+  max-width: 300px;
   border: 2px solid red;
+  
+//   @media ${device.mobileS} { 
+//   min-width: 300px;
+  
+// }
+
+
+// @media ${device.tablet} { 
+//  max-width: 1200px;
+// }
+
+// @media ${device.desktop} {
+//   max-width: 1440px;
+// }
 `;
 
 export const HeroTitle = styled.h1`
-
   font-size: 24px;
 
   margin: 0;
@@ -33,19 +57,19 @@ export const HeroTitle = styled.h1`
 `;
 
 export const HeroText = styled.p`
-font-size: 18px;
-font-weight: 400;
-line-height: 1.33;
-letter-spacing: -0.02em;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.33;
+  letter-spacing: -0.02em;
   color: #b6b6b6;
   margin-top: 16px;
 `;
 
 export const WrapperButtons = styled.div`
-display: flex;
+  display: flex;
 
-gap: 16px;
-margin-top: 24px;
+  gap: 16px;
+  margin-top: 24px;
 `;
 
 export const Button = styled.button`
@@ -89,18 +113,16 @@ export const HeroItem = styled.li`
 `;
 
 export const SVG = styled.svg`
-width: 8px;
-height: 8px;
-margin-top: 4px
+  width: 8px;
+  height: 8px;
+  margin-top: 4px;
 `;
 
 export const HeroItemText = styled.p`
   margin: 0 0 0 8px;
-
 `;
 
- export const WrapperImg = styled.div`
- max-width: 300px;
- border: 2px solid red;
-
- `;
+export const WrapperImg = styled.div`
+  max-width: 300px;
+  border: 2px solid red;
+`;
