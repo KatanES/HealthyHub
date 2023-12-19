0f7353ffd54114a231ef2e8b3c075e4308df2b4f
Diana7119 <123771113+Diana7119@users.noreply.github.com>
2023-12-19 17:20:39 +0100

    2
    2

---
+++ src/components/RecommendedFoodOnMain/RecommendedFoodOnMain.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+import CardRecommendedProduct from 'components/CardRecommendedProduct';
+import {
+  RecommendedFoodSection,
+  TitleSection,
+  RecommendedFoodList,
+  SeeMoreButton,
+} from './RecommendedFoodOnMain.styled';
+
+import { getRecommentedFood } from "helpers/getRecommentedFood";
+import { randomizeFood } from 'helpers/randomizeFood'
+
+const RecommendedFoodOnMain = () => {
+  
+  const [arrayForRender, setArrayForRender] = useState([]);
+  
+  useEffect(() => {
+    getRecommentedFood()
+      .then(responce => {
+        setArrayForRender(randomizeFood(responce))
+      })
+      .catch(err => console.log(err))
+  }, [])
+  
+  return (
+    <RecommendedFoodSection>
+      <TitleSection>Recommended food</TitleSection>
+      <RecommendedFoodList>
+        {arrayForRender.map(({ _id, img, name, amount, calories }) => (
+          <CardRecommendedProduct              
+            key={_id}
+            id={_id}
+            img={img}
+            name={name}
+            amount={amount}
+            calories={calories}
+          />
+        ))}
+      </RecommendedFoodList>
+      <SeeMoreButton to="/recommended-food">See more</SeeMoreButton>
+    </RecommendedFoodSection>
+  );
+};
+
+export default RecommendedFoodOnMain;
