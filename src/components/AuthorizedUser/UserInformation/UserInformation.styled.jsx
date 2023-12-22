d6aaf6f29c7a9a52e1e00eb26ed6784162622841
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-12-22 21:11:48 +0200

    fix package-lock

--- src/components/AuthorizedUser/UserInformation/UserInformation.styled.jsx
+++ src/components/AuthorizedUser/UserInformation/UserInformation.styled.jsx
@@ -6,23 +6,18 @@ export const SettingsContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 0 auto;
-  width: 100%;
   height: 60px;
 
   @media screen and (min-width: 300px) {
-    max-width: 320px;
-    padding: 0 10px;
+    
   }
 
   @media screen and (min-width: 780px) {
-    max-width: 834px;
-    padding: 0 27px;
-    height: 100px;
+   
   }
 
   @media screen and (min-width: 1372px) {
-    max-width: 1440px;
-    padding: 0 34px;
+    
   }
 `;
 
@@ -50,7 +45,8 @@ export const FormLabel = styled.label`
 `; 
 
 export const FormInput = styled.input`
-  padding: 8px 10px;
+padding
+  
   width: 280px;
 
   color: #ffffff;
