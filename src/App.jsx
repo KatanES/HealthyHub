9ec612c606d6c58e50326a09d71a409a6a2b1d27
KatanES <businesskatan.es@gmail.com>
2023-12-23 21:50:41 +0100

    recommended-food + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,80 @@
-import { Route, Routes } from 'react-router-dom';
-// import { PrivateRoute } from './PrivateRoute';
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import { RestrictedRoute } from './ResctrictedRoute';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useEffect, lazy } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './ResctrictedRoute';
 import { AppWrapper } from './App.styled';
-// import { refreshUser } from './redux/auth/operations';
-// import { useAuth } from './components/hooks/useAuth';
+import { refreshUser } from './redux/auth/operations';
+import { useAuth } from './components/hooks/useAuth';
+// import { Loader } from './components/Loader/Loader';
 
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import WelcomePage from './pages/UnAuthorizedUser/WelcomePage/WelcomePage';
-import SignUpPage from './pages/UnAuthorizedUser/SignUpPage/SignUpPage';
-import SignInPage from './pages/UnAuthorizedUser/SignInPage/SignInPage';
-import ForgotPasswordPage from './pages/UnAuthorizedUser/ForgotPasswordPage/ForgotPasswordPage';
+const WelcomePage = lazy(() =>
+  import('./pages/UnAuthorizedUser/WelcomePage/WelcomePage')
+);
+const SignUpPage = lazy(() =>
+  import('./pages/UnAuthorizedUser/SignUpPage/SignUpPage')
+);
+const SignInPage = lazy(() =>
+  import('./pages/UnAuthorizedUser/SignInPage/SignInPage')
+);
+const ForgotPasswordPage = lazy(() =>
+  import('./pages/UnAuthorizedUser/ForgotPasswordPage/ForgotPasswordPage')
+);
+const MainPage = lazy(() => import('./pages/Authorized/MainPage/MainPage'));
+const DashboardPage = lazy(() =>
+  import('./pages/Authorized/DashboardPage/DashboardPage')
+);
+const DiaryPage = lazy(() => import('./pages/Authorized/DiaryPage/DiaryPage'));
+const RecommendedFoodPage = lazy(() =>
+  import('./pages/Authorized/RecommendedFoodPage/RecommendedFoodPage')
+);
+const SettingsPage = lazy(() =>
+  import('./pages/Authorized/SettingsPage/SettingsPage')
+);
+
 import ErrorPage from 'pages/ErrorPage/ErrorPage';
-import MainPage from './pages/Authorized/MainPage/MainPage';
-import DashboardPage from './pages/Authorized/DashboardPage/DashboardPage';
-import DiaryPage from './pages/Authorized/DiaryPage/DiaryPage';
-import RecommendedFoodPage from './pages/Authorized/RecommendedFoodPage/RecommendedFoodPage';
-import SettingsPage from './pages/Authorized/SettingsPage/SettingsPage';
 
 function App() {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <AppWrapper>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<WelcomePage />} />
           <Route path="/welcome" element={<WelcomePage />} />
 
-          <Route path="/signup" element={<SignUpPage />}>
+          <Route
+            path="/signup"
+            element={
+              <RestrictedRoute redirectTo="/" component={<SignUpPage />} />
+            }
+          >
             Sign Up!
           </Route>
-          <Route path="/signin" element={<SignInPage />}>
+          <Route
+            path="/signin"
+            element={
+              <RestrictedRoute redirectTo="/main" component={<SignInPage />} />
+            }
+          >
             Login
           </Route>
           <Route
             path="/forgot-password"
-            element={<ForgotPasswordPage />}
+            element={
+              <RestrictedRoute
+                redirectTo="/signin"
+                component={<ForgotPasswordPage />}
+              />
+            }
           ></Route>
           <Route path="/main" element={<MainPage />}></Route>
           <Route path="/dashboard" element={<DashboardPage />}></Route>
--- src/ResctrictedRoute.jsx
+++ src/ResctrictedRoute.jsx
@@ -1,5 +1,8 @@
 import { useAuth } from './components/hooks/useAuth';
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+export const RestrictedRoute = ({
+  component: Component,
+  redirectTo = '/main',
+}) => {
   const { isSignedIn } = useAuth();
 
   return isSignedIn ? <Navigate to={redirectTo} /> : Component;
