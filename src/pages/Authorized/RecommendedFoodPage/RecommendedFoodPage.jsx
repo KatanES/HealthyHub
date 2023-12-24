0774307bf73f50f91b3bf550ecd50cf38b1b8b3a
KatanES <businesskatan.es@gmail.com>
2023-12-24 10:56:57 +0100

    recommended-food page commented

--- src/pages/Authorized/RecommendedFoodPage/RecommendedFoodPage.jsx
+++ src/pages/Authorized/RecommendedFoodPage/RecommendedFoodPage.jsx
@@ -1,36 +1,36 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecommendedFood } from '../../../redux/recommendedFood/selectors';
-import { fetchRecommendedFood } from '../../../redux/recommendedFood/operations';
-import { RecommendedCard } from '../../../components/AuthorizedUser/RecommendedFood/RecommendedCard';
-RecommendedCard;
-import { randomArray } from '../../../utils/randomArray';
-import RecommendedFoodPhoto from '../../../assets/RecommendedFood/Ketogenic.png';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getRecommendedFood } from '../../../redux/recommendedFood/selectors';
+// import { fetchRecommendedFood } from '../../../redux/recommendedFood/operations';
+// import { RecommendedCard } from '../../../components/AuthorizedUser/RecommendedFood/RecommendedCard';
+// RecommendedCard;
+// import { randomArray } from '../../../utils/randomArray';
+// import RecommendedFoodPhoto from '../../../assets/RecommendedFood/Ketogenic.png';
 
-export const RecommendedFoodPage = () => {
-  const dispatch = useDispatch();
-  const recommendedFood = useSelector(getRecommendedFood);
+// export const RecommendedFoodPage = () => {
+//   const dispatch = useDispatch();
+//   const recommendedFood = useSelector(getRecommendedFood);
 
-  if (!recommendedFood) {
-    dispatch(fetchRecommendedFood());
-  }
+//   if (!recommendedFood) {
+//     dispatch(fetchRecommendedFood());
+//   }
 
-  return (
-    <div>
-      <h1>Recommended Food</h1>
-      <div>
-        <img
-          srcSet={`${RecommendedFoodPhoto} 1x`}
-          src={RecommendedFoodPhoto}
-          alt="Profile Setting Photo"
-        ></img>
-        {recommendedFood && (
-          <ul>
-            {randomArray(recommendedFood, 10).map((product) => (
-              <RecommendedCard key={product.name} info={product} />
-            ))}
-          </ul>
-        )}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Recommended Food</h1>
+//       <div>
+//         <img
+//           srcSet={`${RecommendedFoodPhoto} 1x`}
+//           src={RecommendedFoodPhoto}
+//           alt="Profile Setting Photo"
+//         ></img>
+//         {recommendedFood && (
+//           <ul>
+//             {randomArray(recommendedFood, 10).map((product) => (
+//               <RecommendedCard key={product.name} info={product} />
+//             ))}
+//           </ul>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
