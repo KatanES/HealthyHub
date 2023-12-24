e09417e7be6d38fb97495df629c503870a38f9ef
KatanES <businesskatan.es@gmail.com>
2023-12-24 18:07:05 +0100

    Revert "dashboard"
    This reverts commit 74ad0499a6b49b74b26903cde87f8054a0cf666f.

--- src/pages/Authorized/DashboardPage/DashboardPage.jsx
+++ src/pages/Authorized/DashboardPage/DashboardPage.jsx
@@ -1,47 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { Calories } from './Calories/Calories';
-import { Water } from './Water/Water';
-import { Weight } from './Weight/Weight';
-import { SelectPeriod } from './GetPeriod/GetPeriod';
-
-import { fetchStatistics } from '../../../redux/statistic/operations';
+import { Container } from './DashboardPage.styled';
+import Calories from './Calories/Calories';
+import Water from './Water/Water';
+// import DatePeriod from './GetPeriod/GetPeriod';
 
 const DashboardPage = () => {
-  const [data, setData] = useState({ value: 'lastMonth', label: 'Last Month' });
-
-  const statistics = useSelector((state) => state.statistics.statistics);
-
-  const waterData = statistics?.water[data.value];
-  const caloriesData = statistics?.calories[data.value];
-  const weightData = statistics?.weight[data.value];
-
-  const dispatch = useDispatch();
-
-  const onChange = (dat) => {
-    setData(dat);
-  };
-
-  useEffect(() => {
-    if (!statistics) {
-      dispatch(fetchStatistics());
-    }
-  }, [dispatch, statistics]);
-
   return (
     <>
-      <div>
-        <p onChange={onChange} data={waterData} />
-        <p>
-          <div>
-            <Calories name="Calories" data={caloriesData} period={data} />
-          </div>
-          <div>
-            <Water name="Water" data={waterData} period={data} />
-          </div>
-        </p>
-        <Weight name="Weight" data={weightData} period={data} />
-      </div>
+      {/* <DatePeriod /> */}
+      <Container>
+        <Calories />
+        <Water />
+      </Container>
     </>
   );
 };
--- src/pages/Authorized/DashboardPage/GetPeriod/GetPeriod.jsx
+++ src/pages/Authorized/DashboardPage/GetPeriod/GetPeriod.jsx
@@ -1,103 +1,31 @@
-import { Link } from 'react-router-dom';
-import Select from 'react-select';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import { useState } from 'react';
-import { getMonthOrYear } from '../../../../utils/Dashboard/GetMonthOrYear';
+import { DataBtn, CalendarGlobalStyles } from './GetPeriod.styled';
 
-const GetPeriod = ({ onChange, data }) => {
-  const [toggleSelect, setToggleSelect] = useState({
-    value: 'lastYear',
-    label: 'Last Year',
-  });
+const DatePeriod = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
 
-  const changeOption = () => {
-    if (toggleSelect.value === 'lastMonth') {
-      setToggleSelect({
-        value: 'lastYear',
-        label: 'Last Year',
-      });
-    }
-    if (toggleSelect.value === 'lastYear') {
-      setToggleSelect({
-        value: 'lastMonth',
-        label: 'Last Month',
-      });
-    }
-    onChange(toggleSelect);
-
-    return;
-  };
-
-  const options = [toggleSelect];
+  const CustomInput = ({ value, onClick }) => (
+    <DataBtn className="custom-datepicker-input" onClick={onClick}>
+      {value || 'Select Month'}
+    </DataBtn>
+  );
 
   return (
-    <div>
-      <div>
-        <Link to={'/'}>
-          <ArrowWrap
-            style={{
-              transform: 'rotate(180deg)',
-            }}
-          />
-        </Link>
-        <Select
-          defaultValue={{ value: 'lastMonth', label: 'Last Month' }}
-          onChange={() => changeOption()}
-          menuPosition="fixed"
-          options={options}
-          styles={{
-            control: (baseStyles) => ({
-              ...baseStyles,
-              backgroundColor: 'rgba(5, 5, 5, 1)',
-              border: 'none',
-              placeHolder: 'none',
-            }),
-            indicatorSeparator: () => ({
-              display: 'none',
-            }),
-
-            option: () => ({
-              width: '212px',
-              height: '64px',
-              margin: '0',
-              paddingLeft: '12px',
-              display: 'flex',
-              alignItems: 'center',
-              position: 'relative',
-              borderRadius: '12px',
-              backgroundColor: 'rgba(5, 5, 5, 1)',
-            }),
-            menu: () => ({
-              width: '212px',
-              height: '64px',
-              borderRadius: '12px',
-              boxShadow: '0px 4px 14px 0px rgba(227, 255, 168, 0.20)',
-              cursor: 'pointer',
-              position: 'absolute',
-            }),
-            singleValue: () => ({
-              margin: '0',
-            }),
-            valueContainer: () => ({
-              padding: '0px',
-            }),
-
-            dropdownIndicator: (baseStyles) => ({
-              color: 'rgba(227, 255, 168, 1)',
-              marginLeft: '12px',
-            }),
-            placeholder: () => ({
-              display: 'none',
-            }),
-            input: () => ({
-              display: 'none',
-              padding: '0px',
-            }),
-          }}
-        />
-      </div>
-      <h2>{data && getMonthOrYear(data, toggleSelect.value)}</h2>
-    </div>
+    <>
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+        dateFormat="MMMM"
+        showMonthYearPicker
+        customInput={<CustomInput />}
+        //   calendarClassName="horizontal-calendar"
+      />
+      <CalendarGlobalStyles />
+    </>
   );
 };
 
-export default GetPeriod;
+export default DatePeriod;
